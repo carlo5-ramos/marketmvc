7c569635c94c1f90496e24eea6b49dccbe79a27a
carlo5-ramos <carlos.j11.cr@gmail.com>
2019-10-23 10:34:06 -0600

    arreglando algunos data anotations

--- Market/Models/Employee.cs
+++ Market/Models/Employee.cs
@@ -19,23 +19,23 @@ namespace Market.Models
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "You must enter {0}")]
-        [DisplayFormat(DataFormatString = "0:C2", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Salary { get; set; }
 
         [Display(Name = "Bonus Percent")]
-        [DisplayFormat(DataFormatString = "0:P2", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:P2}", ApplyFormatInEditMode = false)]
 
         public float BonusPercent { get; set; }
 
         [Display(Name = "Date Of Birth")]
         [Required(ErrorMessage = "You must enter {0}")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "0:yyyy/MM/dd", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
 
         [Display(Name = "Start Time")]
         [Required(ErrorMessage = "You must enter {0}")]
-        [DisplayFormat(DataFormatString = "0:hh:mm", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Time)]
         public DateTime StartTime { get; set; }
 
