0a2e4178f00b71ea99247a3fb3fe39fc5d29a222
carlo5-ramos <carlos.j11.cr@gmail.com>
2019-10-30 11:59:23 -0600

    video 23 finalizado

--- Market/Controllers/ProductsController.cs
+++ Market/Controllers/ProductsController.cs
@@ -10,6 +10,7 @@ using Market.Models;
 
 namespace Market.Controllers
 {
+    
     public class ProductsController : Controller
     {
         private MarketContext db = new MarketContext();
@@ -36,6 +37,7 @@ namespace Market.Controllers
         }
 
         // GET: Products/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -59,6 +61,7 @@ namespace Market.Controllers
         }
 
         // GET: Products/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,6 +93,7 @@ namespace Market.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
