a44ccd31cc36a1179d8e4d5dc8c76cf0ac0eaf7f
carlo5-ramos <carlos.j11.cr@gmail.com>
2019-11-04 11:59:47 -0600

    creacion del modeo userview para listar los susarios que estan bd

---
+++ Market/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Market.Models;
+using Market.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Market.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var usersView = new List<UserView>();
+            foreach (var user in users)
+            {
+                var userView = new UserView
+                {
+                    EMail = user.Email,
+                    Name = user.UserName,
+                    UserID = user.Id
+                };
+                usersView.Add(userView);
+            }
+            return View(usersView);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Market/ViewModels/RoleView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Market.ViewModels
+{
+    public class RoleView
+    {
+        public string RolID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
