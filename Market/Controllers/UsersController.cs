e62e61a249940c6cd809f1905d27fc1251dc2a01
carlo5-ramos <carlos.j11.cr@gmail.com>
2019-11-11 12:01:51 -0600

    agregando roles a los usarios video 31

--- Market/Controllers/UsersController.cs
+++ Market/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -35,14 +36,24 @@ namespace Market.Controllers
 
         public ActionResult Roles(string userID)
         {
-            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            if (string.IsNullOrEmpty(userID))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 
-            var roles = roleManager.Roles.ToList();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
             var users = userManager.Users.ToList();
             var user = users.Find(u => u.Id == userID);
 
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var roles = roleManager.Roles.ToList();
             var rolesView = new List<RoleView>();
+
             foreach (var item in user.Roles)
             {
                 var role = roles.Find(r => r.Id == item.RoleId);
@@ -63,6 +74,95 @@ namespace Market.Controllers
             };
             return View(userView);
         }
+
+        public ActionResult AddRole(string userID)
+        {
+            if (string.IsNullOrEmpty(userID))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userID);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            var userView = new UserView
+            {
+                EMail = user.Email,
+                Name = user.UserName,
+                UserID = user.Id
+            };
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var list = roleManager.Roles.ToList();
+            list.Add(new IdentityRole { Id = "", Name = "[Seleccione un role...]" });
+            list = list.OrderBy(r => r.Name).ToList();
+            ViewBag.RoleID = new SelectList(list, "Id", "Name");
+
+            return View(userView);
+        }
+
+        [HttpPost]
+        public ActionResult AddRole(string userID, FormCollection form)
+        {
+            var roleID = Request["RoleID"];
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userID);
+
+            var userView = new UserView
+            {
+                EMail = user.Email,
+                Name = user.UserName,
+                UserID = user.Id
+            };
+
+            if (string.IsNullOrEmpty(roleID))
+            {
+                ViewBag.Error = "You must select a role";
+                var list = roleManager.Roles.ToList();
+                list.Add(new IdentityRole { Id = "", Name = "[Seleccione un role...]" });
+                list = list.OrderBy(r => r.Name).ToList();
+                ViewBag.RoleID = new SelectList(list, "Id", "Name");
+
+                return View(userView);
+            }
+
+            var roles = roleManager.Roles.ToList();
+            var role = roles.ToList().Find(r => r.Id == roleID);
+            if (!userManager.IsInRole(userID, role.Name))
+            {
+                userManager.AddToRole(userID, role.Name);
+            }
+
+            var rolesView = new List<RoleView>();
+            foreach (var item in user.Roles)
+            {
+                role = roles.Find(r => r.Id == item.RoleId);
+                var roleView = new RoleView
+                {
+                    RoleID = role.Id,
+                    Name = role.Name
+                };
+                rolesView.Add(roleView);
+            }
+
+            userView = new UserView
+            {
+                EMail = user.Email,
+                Name = user.UserName,
+                UserID = user.Id,
+                Roles = rolesView
+            };
+            return View("Roles", userView);
+        }
+
 
         protected override void Dispose(bool disposing)
         {
--- Market/ViewModels/UserView.cs
+++ Market/ViewModels/UserView.cs
@@ -15,6 +15,8 @@ namespace Market.ViewModels
         [DataType(DataType.EmailAddress)]
         public string EMail { get; set; }
 
+        public RoleView Role { get; set; }
+
         public List<RoleView> Roles { get; set; }
     }
 }
